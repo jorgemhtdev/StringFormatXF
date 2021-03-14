08e1c1fb3f6d3bcb9b3e32422b5e00f4bd31691f
jorgemhtdev <jorge@jorgemht.dev>
2021-03-14 20:14:58 +0100

    [Added] - Project

---
+++ StringFormatXF/StringFormatXF/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace StringFormatXF
+{
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ StringFormatXF/StringFormatXF/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StringFormatXF
+{
+    using System;
+
+    public class MainPageViewModel
+    {
+
+        public int Number { get; set; } =  100;
+        public DateTime Date { get; set; } =  DateTime.Now;
+    }
+}
